09de6556309a65d1e62ea9c626183939479b4a9d
AMatlak <aleksy.matlak@city.ac.uk>
2023-03-29 21:01:04 +0100

    Changed the Size of the LoginWindow page.

--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -3,46 +3,46 @@ import java.awt.*;
 
 public class LoginWindow {
     public static void main(String[] args) {
-        // We have created an JFrame object
+        //We have created an JFrame object
         JFrame frame = new JFrame("Login Window");
 
-        // We have ajusted the FlowLayout
+        //We have adjusted the FlowLayout
         frame.setLayout(new FlowLayout());
 
         //We have created the JLabel object
         JLabel userLabel = new JLabel("Username:");
         JLabel passLabel = new JLabel("Password:");
 
-        userLabel.setLocation(300, 300);
-        passLabel.setLocation(250,250);
-
-        // We have created the JTextField objects
+        //We have created the JTextField objects
         JTextField userField = new JTextField(20);
         JTextField passField = new JTextField(20);
 
-        // This is the ok button
+        //This is the ok button
         JButton okButton = new JButton("OK");
 
-        // We have created the JPanel object
+        //We have created the JPanel object
         JPanel panel = new JPanel();
 
-        // Assembled all the components into the panel
+        //Assembled all the components into the panel
         panel.add(userLabel);
         panel.add(userField);
         panel.add(passLabel);
         panel.add(passField);
         panel.add(okButton);
 
-        // insert the panel in our frame
+        //serLabel.setLocation(5, 5);
+        //.setLocation(250,250);
+        //okButton.setLocation(100,100);
+        //insert the panel in our frame
         frame.add(panel);
 
-        // frame size
-        frame.setSize(600, 600);
+        //frame size
+        frame.setSize(1000, 800);
 
-        // frame close operation
+        //frame close operation
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // make frame visible
+        //make frame visible
         frame.setVisible(true);
     }
 }
