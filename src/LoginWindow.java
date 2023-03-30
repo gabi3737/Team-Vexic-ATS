34fdfa1325977ef8b80b64fbdeba9fd3ffab3690
AMatlak <aleksy.matlak@city.ac.uk>
2023-03-30 16:31:56 +0100

    Alex and Sany reworked the whole code so its more optimised and also added a LoginWindow Form.

--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -1,48 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class LoginWindow {
+public class LoginWindow extends JFrame {
+    private JButton ExitButton;
+    private JButton LoginButton;
+    private JTextField UserTextField;
+    private JLabel PasswordLabel;
+    private JLabel UserLabel;
+    private JTextField textField1;
+    private JPanel mainPanel;
+
+    public LoginWindow() {
+        setContentPane(mainPanel);
+        setTitle("Vexic");
+        setSize(600,500);
+        mainPanel.setBackground(Color.GREEN);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+
+
     public static void main(String[] args) {
-        //We have created an JFrame object
-        JFrame frame = new JFrame("Login Window");
-
-        //We have adjusted the FlowLayout
-        frame.setLayout(new FlowLayout());
-
-        //We have created the JLabel object
-        JLabel userLabel = new JLabel("Username:");
-        JLabel passLabel = new JLabel("Password:");
-
-        //We have created the JTextField objects
-        JTextField userField = new JTextField(20);
-        JTextField passField = new JTextField(20);
-
-        //This is the ok button
-        JButton okButton = new JButton("OK");
-
-        //We have created the JPanel object
-        JPanel panel = new JPanel();
-
-        //Assembled all the components into the panel
-        panel.add(userLabel);
-        panel.add(userField);
-        panel.add(passLabel);
-        panel.add(passField);
-        panel.add(okButton);
-
-        //serLabel.setLocation(5, 5);
-        //.setLocation(250,250);
-        //okButton.setLocation(100,100);
-        //insert the panel in our frame
-        frame.add(panel);
-
-        //frame size
-        frame.setSize(1000, 800);
-
-        //frame close operation
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //make frame visible
-        frame.setVisible(true);
+        LoginWindow myFrame = new LoginWindow();
     }
 }
